e5f2adfcff4b831e82b53ad8c6c4bf5724390c57
aqseeh <aqsazaheer7636@gmail.com>
2024-09-27 18:55:43 +0500

    avatar added

--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -1,34 +1,28 @@
 import { Link } from "react-router-dom";
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("my-token");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  return isAuthenticated;
+};
 
 const NavBar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef(null); // Reference for the dropdown menu
-
-  // Function to toggle dropdown menu
-  const toggleDropdown = () => {
-    setIsDropdownOpen((prev) => !prev);
-  };
+  const isAuthenticated = useAuth();
 
   // Function to handle link click and close dropdown
   const handleLinkClick = () => {
     setIsDropdownOpen(false);
   };
-
-  // Close dropdown when clicking outside of it
-  const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setIsDropdownOpen(false);
-    }
-  };
-
-  // Adding event listener on component mount
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   return (
     <div className="container mx-auto fixed top-0 z-50 flex justify-between items-center">
@@ -43,7 +37,6 @@ const NavBar = () => {
             >
               Home
             </Link>
-
             <Link
               to="/services"
               className="text-white hover:text-yellow-500"
@@ -51,21 +44,17 @@ const NavBar = () => {
             >
               Services
             </Link>
-
             {/* Dropdown for Cars */}
-            <div className="relative" ref={dropdownRef}>
+            <div className="relative">
               <button
-                onClick={toggleDropdown}
+                onClick={() => setIsDropdownOpen((prev) => !prev)}
                 className="text-white flex items-center hover:text-yellow-500 focus:outline-none"
               >
                 Cars
-                {/* Font Awesome Dropdown Arrow */}
                 <span className="ml-1 text-xs">
                   <i className="fa fa-caret-down text-xs"></i>
                 </span>
               </button>
-
-              {/* Dropdown menu */}
               {isDropdownOpen && (
                 <div className="absolute top-full left-0 mt-2 w-48 bg-black shadow-lg">
                   <Link
@@ -85,14 +74,6 @@ const NavBar = () => {
                 </div>
               )}
             </div>
-
-            <Link
-              to="/reviews"
-              className="text-white hover:text-yellow-500"
-              onClick={handleLinkClick}
-            >
-              Reviews
-            </Link>
             <Link
               to="/about"
               className="text-white hover:text-yellow-500"
@@ -107,6 +88,20 @@ const NavBar = () => {
             >
               Contact Us
             </Link>
+            {isAuthenticated ? (
+              <Avatar>
+                <AvatarImage src="/pictures/audi1.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            ) : (
+              <Link
+                to="/auth/login"
+                className="text-white hover:text-yellow-500"
+                onClick={handleLinkClick}
+              >
+                Sign Up
+              </Link>
+            )}
           </div>
         </div>
       </div>
