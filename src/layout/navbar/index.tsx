39c37cb6eb4e55986ce922bef681bb69a840af6e
kainat333 <kainatbibi2412@gmail.com>
2024-09-27 19:52:05 +0500

    tweak

--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const useAuth = () => {
